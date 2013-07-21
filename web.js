b0fa389b8a6ef5bb1f73d9591ef2cf0817c8575a
Ubuntu <ubuntu@ip-172-31-5-104.us-west-2.compute.internal>
2013-07-21 21:46:25 +0000

    hw4 modify index.html and listen on 8080 web.js

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
-var express = require('express'),
-	app = express.createServer(express.logger()),
-	fs = require('fs'),
-	http = require('http');
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.write(fs.readFileSync("./index.html", "utf8").toString());
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
